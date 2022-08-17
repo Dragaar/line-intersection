097092974821aa2272e3ac57a92dfe1f10db2544
Dragaar <rosivanyshyn@gmail.com>
2022-08-17 20:29:32 +0300

    111

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,20 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
-
+    public int k, b;
     public Line(int k, int b) {
-
+    this.k = k;
+    this.b = b;
     }
 
-    public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+    public Point intersection(Line line2) {
+        if(this.k == line2.k) return null;
+        double x = (line2.b - this.b) / (this.k - line2.k);
+        double y = this.k * x + this.b;
+        Point res = new Point((int) x, (int) y);
+        System.out.println(res.toString());
+        return res;
+       // throw new UnsupportedOperationException();
     }
 
 }
